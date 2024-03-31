7f99baa7dc9789e4479d09eda9ef4044eaccfaf3
Nikita <fen1kser@yandex.ru>
2024-04-01 00:00:04 +0300

    Сделал вместо второго экрана алерт

--- TaskListApp/TaskListApp/SupportingFiles/SceneDelegate.swift
+++ TaskListApp/TaskListApp/SupportingFiles/SceneDelegate.swift
@@ -20,7 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
 
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
